4bc19a09e1fb79e39085fdf02a6fcd0172a75f6a
Your Name <you@example.com>
2022-06-13 17:06:49 -0400

    Change

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import { Operators } from "./Components/Operators";
 import { Display } from "./Components/Display";
 import { MemoryDisplay } from "./Components/MemoryDisplay";
-import "./index.css";
+import './index.css';
 import { useState } from "react";
-import Swal from "sweetalert2";
+
 
 function App() {
   const [currentDisplay, setCurrentDisplay] = useState(0);
   const [memoryDisplay, setMemoryDisplay] = useState("");
-  console.log(currentDisplay[1]);
+
   let prevDisplay = 0;
 
   const alertLimit = () => {
@@ -19,25 +19,35 @@ function App() {
     }, 1000);
   };
 
+  const separator = (num) => {
+    var str = num.toString().split(".");
+    str[0] = str[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    return str.join(".");
+  };
+
   const pressNum = (e) => {
     if (currentDisplay !== "DIGIT LIMIT") {
       if (currentDisplay === 0) {
         setCurrentDisplay(e.target.value);
+        setMemoryDisplay(e.target.value);
       } else if (currentDisplay.length > 14) {
         prevDisplay = currentDisplay;
         alertLimit();
       } else if (
-        currentDisplay.length === 3 ||
-        currentDisplay.length === 7 ||
-        currentDisplay.length === 11
+        currentDisplay === "%" ||
+        currentDisplay === "/" ||
+        currentDisplay === "*" ||
+        currentDisplay === "+"
       ) {
-        //move coma to the right
-           setCurrentDisplay(currentDisplay[0] + ',' + currentDisplay);
-        
-        }
-        else {
+        setMemoryDisplay(memoryDisplay + e.target.value);
+        setCurrentDisplay(e.target.value);
+      } else if (memoryDisplay.charAt(0) === "0" && e.target.value === "0") {
+        setCurrentDisplay(currentDisplay);
+        setMemoryDisplay(memoryDisplay);
+      } else {
+        //add num and move coma to the right
         setCurrentDisplay(currentDisplay + e.target.value);
-        
+        setMemoryDisplay(memoryDisplay + currentDisplay + e.target.value);
       }
     }
   };
@@ -45,35 +55,114 @@ function App() {
   const clearDisplay = () => {
     if (currentDisplay !== "DIGIT LIMIT") {
       setCurrentDisplay(0);
+      setMemoryDisplay("");
     }
   };
 
-  const changeSimbol = () => {
+
+  const porcentage = () => {
     if (currentDisplay !== "DIGIT LIMIT") {
-      if (currentDisplay.charAt(0) === "-") {
-        setCurrentDisplay(currentDisplay.slice(1));
+      if (currentDisplay === 0) {
+        setCurrentDisplay(currentDisplay);
+      } else if (
+        currentDisplay !== 0 &&
+        currentDisplay !== "%" &&
+        currentDisplay !== "/" &&
+        currentDisplay !== "*" &&
+        currentDisplay !== "-" &&
+        currentDisplay !== "+"
+      ) {
+        setMemoryDisplay(memoryDisplay + "%");
+        setCurrentDisplay("%");
       } else {
-        setCurrentDisplay("-" + currentDisplay);
+        setCurrentDisplay("%");
       }
     }
   };
 
-  const porcentage = () => {
+  const divide = () => {
     if (currentDisplay !== "DIGIT LIMIT") {
-      if (currentDisplay !== 0) {
-        setCurrentDisplay(currentDisplay + "%");
+      if (currentDisplay === 0) {
+        setCurrentDisplay(currentDisplay);
+      } else if (
+        currentDisplay !== 0 &&
+        currentDisplay !== "%" &&
+        currentDisplay !== "/" &&
+        currentDisplay !== "*" &&
+        currentDisplay !== "-" &&
+        currentDisplay !== "+"
+      ) {
+        setMemoryDisplay(memoryDisplay + "/");
+        setCurrentDisplay("/");
+      } else {
+        setCurrentDisplay("/");
+      }
+    }
+  };
+
+  const multiply = () => {
+    if (currentDisplay !== "DIGIT LIMIT") {
+      if (currentDisplay === 0) {
+        setCurrentDisplay(currentDisplay);
+      } else if (
+        currentDisplay !== 0 &&
+        currentDisplay !== "%" &&
+        currentDisplay !== "/" &&
+        currentDisplay !== "*" &&
+        currentDisplay !== "-" &&
+        currentDisplay !== "+"
+      ) {
+        setMemoryDisplay(memoryDisplay + "*");
+        setCurrentDisplay("*");
+      } else {
+        setCurrentDisplay("*");
+      }
+    }
+  };
+
+  const subtract = () => {
+    if (currentDisplay !== "DIGIT LIMIT") {
+      if (currentDisplay === 0) {
+        setCurrentDisplay(currentDisplay);
+      } else if (
+        currentDisplay !== 0 &&
+        currentDisplay !== "%" &&
+        currentDisplay !== "/" &&
+        currentDisplay !== "*" &&
+        currentDisplay !== "-" &&
+        currentDisplay !== "+"
+      ) {
+        setMemoryDisplay(memoryDisplay + "-");
+        setCurrentDisplay("-");
+      } else {
+        setCurrentDisplay("-");
+      }
+    }
+  };
+
+  const sum = () => {
+    if (currentDisplay !== "DIGIT LIMIT") {
+      if (currentDisplay === 0) {
+        setCurrentDisplay(currentDisplay);
+      } else if (
+        currentDisplay !== 0 &&
+        currentDisplay !== "%" &&
+        currentDisplay !== "/" &&
+        currentDisplay !== "*" &&
+        currentDisplay !== "-" &&
+        currentDisplay !== "+"
+      ) {
+        setMemoryDisplay(memoryDisplay + "+");
+        setCurrentDisplay("+");
+      } else {
+        setCurrentDisplay("+");
       }
     }
   };
 
   const addDecimal = () => {
     if (currentDisplay !== "DIGIT LIMIT") {
-      if (currentDisplay.includes(".")) {
-        Swal.fire({
-          title: "Decimal already exists",
-          icon: "info",
-        });
-      } else {
+      if (!currentDisplay.includes(".")) {
         setCurrentDisplay(currentDisplay.concat("."));
       }
     }
@@ -82,25 +171,37 @@ function App() {
   const deleteLastNum = () => {
     if (currentDisplay !== "DIGIT LIMIT") {
       if (currentDisplay.length > 1) {
-        setCurrentDisplay(currentDisplay.slice(1));
+        setCurrentDisplay(currentDisplay.slice(0, -1));
       } else {
         setCurrentDisplay(0);
       }
     }
   };
+const isOperator = /[*%/+‑]/,
+    endsWithOperator = /[*%+‑/]$/,
+    endsWithNegativeSign = /\d[*%/+‑]{1}‑$/;
+
+  const equal = () => {
+    
+
+  };
 
   return (
     <div id="app">
       <div id="calculator">
-        <MemoryDisplay memoryDisplay={memoryDisplay} />
-        <Display currentDisplay={currentDisplay} />
+        <MemoryDisplay memoryDisplay={separator(memoryDisplay)} />
+        <Display currentDisplay={separator(currentDisplay)} />
         <Operators
           pressNum={pressNum}
           clearDisplay={clearDisplay}
           deleteLastNum={deleteLastNum}
-          changeSimbol={changeSimbol}
           addDecimal={addDecimal}
           porcentage={porcentage}
+          divide={divide}
+          subtract={subtract}
+          sum={sum}
+          multiply={multiply}
+          equal={equal}
         />
       </div>
     </div>
--- src/Components/Operators.js
+++ src/Components/Operators.js
@@ -1,126 +1,211 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBackspace } from "@fortawesome/free-solid-svg-icons";
 
+
 const Operators = ({
   pressNum,
   clearDisplay,
   deleteLastNum,
-  changeSimbol,
   addDecimal,
   porcentage,
+  divide,
+  subtract,
+  sum,
+  multiply,
+  equal,
 }) => {
   return (
-    <div id="operators">
-      <div>
-        <button id="clear" className="operatorGray" onClick={clearDisplay}>
-          C
-        </button>
-        <button id="simbol" className="operatorGray" onClick={changeSimbol}>
-          +/-
-        </button>
-        <button id="portentage" className="operatorGray" onClick={porcentage}>
-          %
-        </button>
-        <button id="divide" className="operatorOrange">
+    <div id="container">
+      
+        <div>
+          <button id="clear" className="operatorGray" onClick={clearDisplay}>
+            C
+          </button>
+        </div>
+        <div>
+          <button id="remove" className="operatorGray" onClick={deleteLastNum}>
+            <FontAwesomeIcon icon={faBackspace} />
+          </button>
+        </div>
+        <div>
+        <button id="divide" className="operatorOrange" onClick={divide}>
           /
         </button>
-      </div>
-      <div>
-        <button
-          id="seven"
-          className="operatorBlack"
-          value="7"
-          onClick={pressNum}
-        >
-          7
-        </button>
-        <button
-          id="eight"
-          className="operatorBlack"
-          value="8"
-          onClick={pressNum}
-        >
-          8
-        </button>
-        <button
-          id="nine"
-          className="operatorBlack"
-          value="9"
-          onClick={pressNum}
-        >
-          9
-        </button>
-        <button id="multiply" className="operatorOrange" value="x">
-          x
-        </button>
-      </div>
-      <div>
-        <button
-          id="four"
-          className="operatorBlack"
-          value="4"
-          onClick={pressNum}
-        >
-          4
-        </button>
-        <button
-          id="five"
-          className="operatorBlack"
-          value="5"
-          onClick={pressNum}
-        >
-          5
-        </button>
-        <button id="six" className="operatorBlack" value="6" onClick={pressNum}>
-          6
-        </button>
-        <button id="subtract" className="operatorOrange" value="-">
-          -
-        </button>
-      </div>
-      <div>
-        <button id="one" className="operatorBlack" value="1" onClick={pressNum}>
-          1
-        </button>
-        <button id="two" className="operatorBlack" value="2" onClick={pressNum}>
-          2
-        </button>
-        <button
-          id="three"
-          className="operatorBlack"
-          value="3"
-          onClick={pressNum}
-        >
-          3
-        </button>
-        <button id="add" className="operatorOrange" value="+">
-          +
-        </button>
-      </div>
-      <div>
-        <button
-          id="zero"
-          className="operatorBlack"
-          value="0"
-          onClick={pressNum}
-        >
-          0
-        </button>
-        <button
-          id="decimal"
-          className="operatorBlack"
-          value="."
-          onClick={addDecimal}
-        >
-          .
-        </button>
-        <button id="remove" className="operatorBlack" onClick={deleteLastNum}>
-          <FontAwesomeIcon icon={faBackspace} />
-        </button>
-        <button id="equals" className="operatorOrange" value="=">
-          =
-        </button>
-      </div>
+        </div>
+        <div>
+          <button id="portentage" className="operatorOrange" onClick={porcentage}>
+            %
+          </button>
+        </div>
+     
+
+     
+        <div>
+          <button
+            id="seven"
+            className="operatorBlack"
+            value="7"
+            onClick={pressNum}
+          >
+            7
+          </button>
+        </div>
+
+        <div>
+          <button
+            id="eight"
+            className="operatorBlack"
+            value="8"
+            onClick={pressNum}
+          >
+            8
+          </button>
+        </div>
+
+        <div>
+          <button
+            id="nine"
+            className="operatorBlack"
+            value="9"
+            onClick={pressNum}
+          >
+            9
+          </button>
+        </div>
+
+        <div>
+          <button
+            id="multiply"
+            className="operatorOrange"
+            value="."
+            onClick={multiply}
+          >
+            x
+          </button>
+        </div>
+      
+
+      
+        <div>
+          <button
+            id="four"
+            className="operatorBlack"
+            value="4"
+            onClick={pressNum}
+          >
+            4
+          </button>
+        </div>
+
+        <div>
+          <button
+            id="five"
+            className="operatorBlack"
+            value="5"
+            onClick={pressNum}
+          >
+            5
+          </button>
+        </div>
+
+        <div>
+          <button
+            id="six"
+            className="operatorBlack"
+            value="6"
+            onClick={pressNum}
+          >
+            6
+          </button>
+        </div>
+
+        <div>
+          <button
+            id="subtract"
+            className="operatorOrange"
+            value="-"
+            onClick={subtract}
+          >
+            -
+          </button>
+        </div>
+    
+
+     
+        <div>
+          <button
+            id="one"
+            className="operatorBlack"
+            value="1"
+            onClick={pressNum}
+          >
+            1
+          </button>
+        </div>
+
+        <div>
+          <button
+            id="two"
+            className="operatorBlack"
+            value="2"
+            onClick={pressNum}
+          >
+            2
+          </button>
+        </div>
+
+        <div>
+          <button
+            id="three"
+            className="operatorBlack"
+            value="3"
+            onClick={pressNum}
+          >
+            3
+          </button>
+        </div>
+
+        <div>
+          <button id="add" className="operatorOrange" value="+" onClick={sum}>
+            +
+          </button>
+        </div>
+      
+
+     
+        <div  >
+          <button
+            id="zero"
+            className="operatorBlack"
+            value="0"
+            onClick={pressNum}
+          >
+            0
+          </button>
+        </div>
+
+        <div >
+          <button
+            id="decimal"
+            className="operatorBlack"
+            value="."
+            onClick={addDecimal}
+          >
+            .
+          </button>
+        </div>
+
+        <div className="jumboEqual" >
+          <button
+            id="equals"
+            className="operatorOrange"
+            value="="
+            onClick={equal}
+          >
+            =
+          </button>
+        </div>
+      
     </div>
   );
 };
